8ac1c3393bda8d1bb61cd689b26e476c393bf840
Francisca Verdugo <fran.veriffo@gmail.com>
2024-12-13 21:14:07 -0300

    modificaciones en las cards

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -4,10 +4,12 @@ const CardPizza = ({ name, price, ingredients, img }) => (
             <img src={img} className="card-img-top" alt={name} />
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
+                <hr style={{ border: '1px solid #ccc' }} />
                 <p>Ingredientes: </p>
-                     <p>{ingredients.join(', ')}</p>
+                     <p className="card-ingredients">{ingredients.join(', ')}</p>
+                <hr style={{ border: '1px solid #ccc' }} />
                 <p className="card-text">Precio: ${price.toLocaleString()}</p>
-                <button className="btn btn-light me-2">Ver más</button>
+                <button className="btn btn-light button-spacing">Ver más</button>
                 <button className="btn btn-dark">Añadir 🛒</button>
             </div>
         </div>
